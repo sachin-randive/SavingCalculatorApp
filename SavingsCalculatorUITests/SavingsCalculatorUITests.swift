67cfcb0f8762a6fd9e91a8fc0a69fb92eb822d9b
Sachin Randive <r.sachin81@gmail.com>
2025-12-22 17:30:55 +0530

    Added UI testing

--- SavingsCalculatorUITests/SavingsCalculatorUITests.swift
+++ SavingsCalculatorUITests/SavingsCalculatorUITests.swift
@@ -4,22 +4,17 @@
 //
 //  Created by Sachin Randive on 19/12/25.
 //
-
+import Foundation
 import XCTest
 
 final class SavingsCalculatorUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
     }
 
     @MainActor
@@ -27,15 +22,37 @@ final class SavingsCalculatorUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        XCTAssertTrue(app.navigationBars["Savings Calculator"].waitForExistence(timeout: 3))
+        
+        let initialDeposite = 10_000.0
+        let MonthlyContribution = 200.00
+        let annualRate = 0.037
+        let months = 10*12
+        
+        let monthlyRate = annualRate / 12.0
+        
+        XCTAssertTrue(app.textFields[initialDeposite.formattedAsCurrency()].exists)
+        XCTAssertTrue(app.textFields[MonthlyContribution.formattedAsCurrency()].exists)
+        XCTAssertTrue(app.textFields[String(months / 12)].exists)
+        
+        app.buttons["Calculate"].tap()
+        let resultsNav = app.navigationBars["Results"]
+        XCTAssertTrue(resultsNav.waitForExistence(timeout: 3))
+        
+        XCTAssertTrue(app.staticTexts["Initial Deposit"].exists)
+        XCTAssertTrue(app.staticTexts["Contributions"].exists)
+        XCTAssertTrue(app.staticTexts["Interest Earned"].exists)
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+}
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+extension Double {
+    func formattedAsCurrency() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        return formatter.string(from: NSNumber(value: self)) ?? "\(self)"
     }
 }
